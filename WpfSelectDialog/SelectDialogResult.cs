186cf7de9639c401e417ebdee42cffb1d3ce1a21
Thomas Mentzel <Thomas Mentzel>
2016-01-12 11:51:09 +0100

    support for async threas on startup. issue #6

---
+++ WpfSelectDialog/SelectDialogResult.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Awesome.WpfSelectDialog
+{
+    public class SelectDialogResult<T>
+    {
+        public SelectDialogResult(bool? dialogResult, IEnumerable<T> selectedItems)
+        {
+            DialogResult = dialogResult;
+            SelectedItems = selectedItems;
+        }
+
+        public bool? DialogResult { get; private set; }
+        public IEnumerable<T> SelectedItems { get; private set; }
+    }
+}
---
+++ WpfSelectDialog/WindowExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
+using Awesome.WpfSelectDialog.Controls;
+
+namespace Awesome.WpfSelectDialog
+{
+    public static class WindowExtensions
+    {
+        public static SelectDialogResult<T> ShowDialog<T>(this Func<Window> windowFactory)
+        {
+            var result = new SelectDialogResult<T>(null, null);
+
+            var newWindowThread = new Thread(() =>
+            {
+                var window = windowFactory();
+                window.Closed += (s, e) => Dispatcher.CurrentDispatcher.BeginInvokeShutdown(DispatcherPriority.Background);
+                var dlgResult = window.ShowDialog();
+                result = new SelectDialogResult<T>(dlgResult, null);
+
+                Dispatcher.Run();
+
+                var typedWindow = window as FrameWindow;
+                var content = typedWindow?.Content as ContentFrame;
+                var ctrl = content?.ContentControl as ISelect<T>;
+                if (ctrl == null) return;
+   
+                result = new SelectDialogResult<T>(dlgResult, ctrl.SelectedItems);
+            });
+
+            newWindowThread.SetApartmentState(ApartmentState.STA);
+            newWindowThread.Start();
+            newWindowThread.Join();
+
+            return result;
+        }
+    }
+}
